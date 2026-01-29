9779c449292802991905d365da1e3ca86eb34acb
SHUMIKAZE <shumikaze@proton.me>
2026-01-29 23:05:16 +0300

    refactor: remove import core to utils

--- src/utils/initialization.py
+++ src/utils/initialization.py
@@ -1,8 +1,7 @@
 from sqlite3 import Connection
-from ..core.db.db_api import SQLite3Connection
 from .tables import CREATE_WORKS_SQL
 
 
 def init_db(conn: Connection) -> None:
-    db = SQLite3Connection(conn)
-    db._executescript(CREATE_WORKS_SQL)
+    conn.executescript(CREATE_WORKS_SQL)
+    conn.commit()
